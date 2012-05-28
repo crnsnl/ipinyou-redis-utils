1100428f9370d4e42ec4a1d313f8c3c2c8dedccd
crnsnl <crnsnl@qq.com>
2012-05-28 12:13:38 +0800

    修改artifactId

--- src/main/java/com/ipinyou/redis/aof/AofParser.java
+++ src/main/java/com/ipinyou/redis/aof/AofParser.java
@@ -70,7 +70,6 @@ public class AofParser {
         	}
         	
         	Argument argument = new Argument(args);
-        	System.out.println(argument);
         	redisCommandHandler.exec(argument);
         	
         }
